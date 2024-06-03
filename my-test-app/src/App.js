7ea02413b973e9bcb3d74086a0b8ad8a4bc001aa
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-06-03 21:19:43 +0900

    コンポーネント切り分け

--- my-test-app/src/App.js
+++ my-test-app/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Title from "./component/title/title";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Title />
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ my-test-app/src/component/title/title.jsx
@@ -0,0 +1,5 @@
+function Title() {
+  return <h1>復習</h1>;
+}
+
+export default Title;
