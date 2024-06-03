477e7e44d92a5307123dc2479fbf9c01d61d0981
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-06-03 21:48:25 +0900

    ステート追記＆機能追加

--- my-test-app/src/component/quiz/quiz.jsx
+++ my-test-app/src/component/quiz/quiz.jsx
@@ -8,20 +8,31 @@ function Quiz() {
     setQuiz(event.target.value);
   };
 
+  // フォームの送信時に呼び出される関数
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Question: ${quiz}`);
+    // ここで、クイズの問題を保存する処理を追加できます
+  };
+
   return (
     <div>
-      <textarea
-        value={quiz}
-        onChange={handleInputChange}
-        rows="4"
-        cols="50"
-        placeholder="ここに問題が出題されます"
-      />
-      <div className="button">
-        <button type="submit">1</button>
-        <button type="submit">2</button>
-        <button type="submit">3</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <textarea
+            value={quiz}
+            onChange={handleInputChange}
+            rows="4"
+            cols="50"
+            placeholder="ここに問題が出題されます"
+          />
+        </label>
+        <div className="button">
+          <button type="submit">1</button>
+          <button type="submit">2</button>
+          <button type="submit">3</button>
+        </div>
+      </form>
     </div>
   );
 }
